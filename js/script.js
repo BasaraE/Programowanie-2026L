a9dd181636876f25154328488fb3ae48f0d627d6
Diana Hapiuk <hapiukd@gmail.com>
2026-07-14 16:18:26 +0200

    feat: add form validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 const movies = [];
 
-function addMovie() {
+const form = document.getElementById("movieForm");
+const title = document.getElementById("title");
+const list = document.getElementById("movieList");
+const message = document.getElementById("message");
 
-}
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    if(title.value.trim() === ""){
+        alert("Podaj nazwę filmu!");
+        return;
+    }
+
+    movies.push(title.value);
+
+    const li = document.createElement("li");
+    li.textContent = title.value;
+
+    list.appendChild(li);
+
+    message.textContent = "Film został dodany pomyślnie!";
+
+    title.value = "";
+
+});
